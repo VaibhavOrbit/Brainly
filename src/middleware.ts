2f3951861bfaf12ab5d28030d280a21560928574
VaibhavOrbit <vaibhav.readme@gmail.com>
2025-02-08 12:27:05 +0530

    type fix

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,5 +1,5 @@
 import { NextFunction, Request, Response } from "express";
-import jwt from "jsonwebtoken";
+import jwt, { JwtPayload } from "jsonwebtoken";
 import { JWT_PASSWORD } from "./config";
 
 export const userMiddleware = (req:Request, res:Response, next:NextFunction)=>{
@@ -7,13 +7,21 @@ export const userMiddleware = (req:Request, res:Response, next:NextFunction)=>{
     const decoded = jwt.verify(token as string, JWT_PASSWORD);
 
     if(decoded){
-        // @ts-ignore
-        req.userId = decoded.id;
+
+        if(typeof decoded === "string"){
+            res.status(403).json({
+                message:"You're not logged in"
+            })
+            return;
+        }
+       
+        req.userId = (decoded as JwtPayload ).id;
         next()
-    }
+    } 
     else{
+
         res.status(403).json({
-            message:"You're not signed in"
+            message:"You're not logged in"
         })
     }
 } 
